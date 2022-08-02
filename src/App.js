62fe41715875eb673431b0a0d57ac87784feb4a8
Romina <romina.pati@gmail.com>
2022-08-01 21:50:04 -0400

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React Calculator</h1>
+      <h1>React Calculator Project</h1>
       <Calculator />
     </div>
   );
